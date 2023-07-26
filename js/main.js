60ba522c72419809cd3435dbd0184b187b4d703b
always <wpwc2023@gmail.com>
2023-07-27 01:19:36 +0900

    헤더-검색바script

--- js/main.js
+++ js/main.js
@@ -1,29 +1,55 @@
+
+/*
+ 장바구니
+ */
+// 장바구니 관련 요소 찾기.
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
-basketStarterEl.addEventListener('click', function(event) {
-  event.stopPropagation()
+basketStarterEl.addEventListener('click', event => {
+  event.stopPropagation() // 이벤트 버블링 정지! - 버튼을 클릭했을 때 드롭다운 메뉴가 나타나야 함.
   if (basketEl.classList.contains('show')) {
-    //hide
     hideBasket()
   } else {
-    //show
     showBasket()
   }
 })
-
-basketEl.addEventListener('click', function(event) {
-  event.stopPropagation()
+basketEl.addEventListener('click', event => {
+  event.stopPropagation() // 이벤트 버블링 정지! - 드롭다운 메뉴 영역을 클릭했을 때 메뉴가 사라지지 않아야 함.
 })
-
-window.addEventListener('click', function() {
+// 화면 전체를 클릭했을 때 메뉴가 사라짐.
+window.addEventListener('click', () => {
   hideBasket()
 })
 
+// 특정 로직을 직관적인 함수 이름으로 묶음.
 function showBasket() {
   basketEl.classList.add('show')
 }
-
 function hideBasket() {
   basketEl.classList.remove('show')
+}
+
+/*
+ 헤더 검색
+ */
+// 헤더 검색 관련 요소 찾기.
+const headerEl = document.querySelector('header')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+}
+
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
 }
